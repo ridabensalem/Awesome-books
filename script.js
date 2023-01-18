b704750e1a413ac5b89813209bd12726c349b796
ridabensalem <ridasbnesalmas@gmail.com>
2023-01-18 09:41:44 +0100

    Get the local date and display it

--- script.js
+++ script.js
@@ -61,3 +61,12 @@ book.buttonAdd.addEventListener('click', () => {
   book.bookTitle.value = '';
   book.bookAuthor.value = '';
 });
+// get the current date
+const dateTimeDiv = document.getElementById('DateTime');
+const date = new Date();
+const dateString = date.toLocaleDateString('en-US', { month: 'long', day: 'numeric', year: 'numeric' });
+const timeString = date.toLocaleTimeString('en-US', {
+  hour: 'numeric', minute: 'numeric', second: 'numeric', hour12: true,
+});
+const dateTime = `${dateString} , ${timeString}`;
+dateTimeDiv.innerHTML = dateTime;
